b3336c6708df297ca759d43eb3aa9a2fb0725379
Jorge Yanar <jorgeyanar@gmail.com>
2022-09-29 16:32:33 -0400

    cleanup, add gravity back

--- player.go
+++ player.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -12,8 +13,6 @@ type Player struct {
 	world *World
 	vy    float64
 	speed float64
-	trailx []float64
-	traily []float64
 }
 
 func (p *Player) setPosition(x, y float64) {
@@ -36,22 +35,15 @@ func (p *Player) init(x float64, y float64, w *World) {
 	p.speed = 220
 }
 
-// func (p Player) isGrounded() bool {
-// 	isgrd := p.world.checkIsColliding(&Obj{p.x, p.y + p.h, p.w, 1})
-// 	return isgrd
-// }
+func (p Player) isGrounded() bool {
+	return p.world.checkIsColliding(&Obj{p.x, p.y + p.h, p.w, 1}) != nil
+}
 
 func (p *Player) Update() error {
-	p.trailx = append(p.trailx, p.x)
-	p.traily = append(p.traily, p.y)
 	dt := 0.01
-	dx, dy := 0.0, 0.0
-	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {//&& p.isGrounded() {
-		// p.vy = JUMP_SPEED
-		dy = dy - dt * p.speed
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
-		dy = dy + dt * p.speed
+	dx := 0.0
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && p.isGrounded() {
+		p.vy = JUMP_SPEED
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
 		dx = dx - dt * p.speed
@@ -59,18 +51,14 @@ func (p *Player) Update() error {
 	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
 		dx = dx + dt * p.speed
 	}
-	// p.world.move(p, p.x+dx, p.y+p.vy)
-	p.world.move(p, p.x+dx, p.y+dy)
-	// p.vy = math.Min(p.vy + GRAVITY * dt, 12)
-	// if p.isGrounded() {
-	// 	p.vy = 0
-	// }
+	p.world.move(p, p.x+dx, p.y+p.vy)
+	p.vy = math.Min(p.vy + GRAVITY * dt, 12)
+	if p.isGrounded() {
+		p.vy = 0
+	}
 	return nil
 }
 
 func (p *Player) Draw(screen *ebiten.Image) {
 	ebitenutil.DrawRect(screen, p.x, p.y, p.w, p.h, image.White)
-	// for i, _ := range p.trailx {
-	// 	ebitenutil.DrawCircle(screen, p.trailx[i], p.traily[i], 2, image.White)
-	// }
 }
--- positionalobjects.go
+++ positionalobjects.go
@@ -10,14 +10,6 @@ type PosObj interface {
 	getPosAndSize() (float64, float64, float64, float64)
 }
 
-// type IObj struct {
-// 	Obj
-// }
-
-// func (i IObj) init(X float64, Y float64, w int, h int) IObj {
-// 	return IObj{Pos{X, Y}, w, h}
-// }
-
 func (o *Obj) setPosition(x, y float64) {
 	o.x = x
 	o.y = y
